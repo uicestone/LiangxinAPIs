0c87030db5bde1f4b0316d07ad683377fbe5d6c7
Uice Lu <uicestone@gmail.com>
2015-05-17 00:54:44 +0800

    database structure

---
+++ database.sql
@@ -0,0 +1,108 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.30-log)
+# Database: liangxin
+# Generation Time: 2015-05-16 13:31:42 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table groups
+# ------------------------------------------------------------
+
+CREATE TABLE `groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `members` int(11) unsigned NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `description` text,
+  `leader` varchar(20) DEFAULT NULL,
+  `contact` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `parent_id` int(11) unsigned DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `parent` (`parent_id`),
+  CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `groups` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table posts
+# ------------------------------------------------------------
+
+CREATE TABLE `posts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author_id` int(11) unsigned NOT NULL,
+  `excerpt` text,
+  `content` text,
+  `event_date` date DEFAULT NULL,
+  `event_address` varchar(255) DEFAULT NULL,
+  `event_type` varchar(20) DEFAULT NULL,
+  `class_type` varchar(20) DEFAULT NULL,
+  `banner_position` varchar(20) DEFAULT NULL,
+  `due_date` date DEFAULT NULL,
+  `parent_id` int(11) unsigned DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `group_id` int(11) unsigned DEFAULT NULL,
+  `poster_id` int(11) unsigned DEFAULT NULL,
+  `likes` int(11) unsigned NOT NULL DEFAULT '0',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`),
+  KEY `title` (`title`),
+  KEY `author_id` (`author_id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `group_id` (`group_id`),
+  KEY `poster_id` (`poster_id`),
+  CONSTRAINT `posts_ibfk_4` FOREIGN KEY (`poster_id`) REFERENCES `posts` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`parent_id`) REFERENCES `posts` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `group_id` int(11) unsigned NOT NULL,
+  `password` char(60) DEFAULT NULL,
+  `token` char(60) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `group_id` (`group_id`),
+  KEY `password` (`password`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
