3582ee0c29aff4ae4a7322008f1e1a22d721bf40
Uice Lu <uicestone@gmail.com>
2015-06-14 21:59:18 +0800

    add 'role' attribute for users

--- app/User.php
+++ app/User.php
@@ -22,8 +22,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'token', 'contact', 'avatar'];
-	protected $visible = ['id', 'name', 'contact', 'avatar', 'group_id', 'department_id'];
+	protected $fillable = ['name', 'token', 'contact', 'avatar', 'role'];
+	protected $visible = ['id', 'name', 'contact', 'avatar', 'role', 'group_id', 'department_id'];
 
 	public $timestamps = false;
 
