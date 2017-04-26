1e4fa4c1c31ccafee0bf204e468b2bcfe91dfb24
Uice Lu <uicestone@gmail.com>
2017-04-27 02:04:07 +0800

    党建地图前端欢迎和列表页面

---
+++ resources/assets/js/poi/app.js
@@ -0,0 +1,104 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('liangxin-poi', [
+	'ngRoute',
+	'ui.bootstrap',
+	'liangxin-poi.services',
+	'liangxin-poi.controllers'
+])
+.config(['$routeProvider', '$httpProvider', '$locationProvider', '$sceProvider', function($routeProvider, $httpProvider, $locationProvider, $sceProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'WelcomeController',
+			templateUrl: '../assets/html/poi/welcome.html'
+		})
+		.when('/:category', {
+			controller: 'PoiListController',
+			templateUrl: '../assets/html/poi/list.html',
+			resolve: {
+				pois: ['$route', 'Poi', function($route, Poi) {
+					return Poi.query({class_type: $route.current.params.category, per_page: 999}).$promise;
+				}]
+			}
+		})
+		.when('/:category/:id', {
+			controller: 'PoiDetailController',
+			templateUrl: '../assets/html/poi/detail.html',
+			resolve: {
+				poi: ['$route', 'Poi', function($route, Poi) {
+					return Poi.get({id: $route.current.params.id}).$promise;
+				}]
+			}
+		});
+
+	$httpProvider.interceptors.push('HttpInterceptor');
+	$locationProvider.html5Mode(true);
+	$sceProvider.enabled(false);
+
+}]);
+
+angular.module('liangxin-poi.controllers', [])
+
+.controller('WelcomeController', ['$scope', '$location', '$timeout', function($scope, $location, $timeout){
+
+	$scope.zoom = 1;
+	$scope.windowWidth = window.innerWidth;
+	
+	$scope.zoomIn = function () {
+		$scope.zoomBefore = $scope.zoom;
+		$scope.zoom = $scope.zoom * 1.5;
+		$timeout(function () {
+			$('.map-scroller').scrollLeft($('.map-scroller').scrollLeft() + ($scope.zoom - $scope.zoomBefore) * $scope.windowWidth / 2);
+		});
+	};
+
+	$scope.zoomOut = function () {
+		$scope.zoom = $scope.zoom / 1.5;
+		$timeout(function () {
+			// $('.map-scroller').scrollLeft($('.map-scroller').scrollLeft() - ($scope.zoomBefore - $scope.zoom) * $scope.windowWidth / 2);
+		});
+	};
+
+}])
+
+.controller('PoiListController', ['$scope', '$location', '$route', 'pois', function($scope, $location, $route, pois){
+
+	$scope.pois = pois;
+
+	$scope.category = $route.current.params.category;
+
+	switch ($scope.category) {
+		case 'service-center' : $scope.listTitle = '党建服务分中心分布情况'; break;
+		case 'workshop' : $scope.listTitle = '党代表工作室分布情况'; break;
+		case 'consultant' : $scope.listTitle = '党建顾问分布情况'; break;
+	}
+
+	$scope.showPoiDetail = function (poi) {
+		$location.url($location.path() + '/' + poi.id);
+	};
+
+}])
+
+.controller('PoiDetailController', ['$scope', '$location', 'poi', function($scope, $location, poi){
+
+	$scope.poi = poi;
+
+}])
+
+.controller('AlertController', ['$scope', '$rootScope', 'Alert',
+	function($scope, $rootScope, Alert){
+		$scope.alerts = Alert.get();
+		$scope.close = Alert.close;
+		$scope.previous = function(){};
+		$scope.next = function(){};
+
+		$scope.toggleCloseButton = function(index){
+			$scope.alerts[index].closeable = !$scope.alerts[index].closeable;
+		};
+
+		$rootScope.$on('$routeChangeSuccess', function(){
+			Alert.clear();
+		});
+	}
+]);
---
+++ resources/assets/js/poi/services.js
@@ -0,0 +1,166 @@
+'use strict';
+
+var responseInterceptor = function(response){
+	response.resource.$response = response;
+	return response.resource;
+};
+
+angular.module('liangxin-poi.services', ['ngResource'])
+
+.service('Poi', ['$resource', function($resource){
+	var poi = $resource('../api/v1/post/:id', {id: '@id', type: '地图'}, {
+		query: {method: 'GET', isArray: true, interceptor: {response: responseInterceptor}},
+		create: {method: 'POST'},
+		update: {method: 'PUT'}
+	});
+	
+	poi.prototype.$save = function(a, b, c, d){
+		if(this.id){
+			return this.$update(a, b, c, d);
+		}
+		else{
+			return this.$create(a, b, c, d);
+		}
+	};
+	
+	return poi;
+}])
+
+.service('HttpInterceptor', ['$q', '$timeout', 'Alert', function($q, $timeout, Alert) {
+
+	return {
+		request: function(config) {
+
+			if(config && config.cache === undefined){
+
+				config.alert = {normal: {}, slow: {}};
+
+				config.alert.normal.timeout = $timeout(function(){
+					config.alert.normal.id = Alert.add('正在加载...');
+				}, 200);
+
+				config.alert.slow.timeout = $timeout(function(){
+					Alert.close(config.alert.normal.id);
+					config.alert.slow.id = Alert.add('仍在继续...');
+				}, 5000);
+
+				config.headers['Xinxin-Request-From'] = 'admin';
+				config.headers['Authorization'] = token || (localStorage && localStorage.getItem('token'));
+
+				return config;
+			}
+
+			return config || $q.when(config);
+		},
+		requestError: function(rejection) {
+			return $q.reject(rejection);
+		},
+		response: function(response) {
+
+			if(response && response.config.cache === undefined){
+				$timeout.cancel(response.config.alert.normal.timeout);
+				$timeout.cancel(response.config.alert.slow.timeout);
+				Alert.close(response.config.alert.normal.id);
+				Alert.close(response.config.alert.slow.id);
+			}
+
+			return response || $q.when(response);
+		},
+		responseError: function(rejection) {
+
+			$timeout.cancel(rejection.config.alert.normal.timeout);
+			$timeout.cancel(rejection.config.alert.slow.timeout);
+			Alert.close(rejection.config.alert.normal.id);
+			Alert.close(rejection.config.alert.slow.id);
+
+			if(rejection.status >= 400 && rejection.status < 500){
+				alert(angular.fromJson(rejection.statusText) || rejection.statusText);
+			}
+
+			if(rejection.data.message){
+				Alert.add(rejection.data.message, 'danger', true);
+			}
+			else if(rejection.status > 0){
+				Alert.add(rejection.statusText, 'danger', true);
+			}
+
+			return $q.reject(rejection);
+		}
+	};
+}])
+
+.service('Alert', [function(){
+	
+	var items = [];
+		
+	this.get = function(){
+		return items;
+	};
+
+	this.add = function(message, type) {
+		var id = new Date().getTime();
+		for(var index in items){
+			if (items[index].msg === message){
+				items.splice(index, 1);
+			}
+		}
+		items.push({id: id, msg: message, type: type === undefined ? 'warning' : type});
+		return id;
+	};
+
+	this.close = function(id){
+		if(id === undefined){
+			return;
+		}
+		for(var index in items){
+			if (items.hasOwnProperty(index) && items[index].id === id){
+				break;
+			}
+		}
+		items.splice(index, 1);
+	};
+	
+	this.clear = function(){
+		items.splice(0, items.length);
+	}
+	
+}])
+.service('Bridge', function() {
+	return {
+		_exec: function(method, params, callback){
+			var callbackName = "LiangxinJSCallback_" + (+new Date()) + "_" + Math.floor(Math.random() * 50);
+			var iframe = document.createElement('iframe');
+			window[callbackName] = callback;
+			iframe.src = "js://_?method=" + method + "&params=" + encodeURIComponent(JSON.stringify(params)) + "&callback=" + callbackName;
+			console.log("iframe src", iframe.src);
+			document.body.appendChild(iframe);
+			iframe.style.display = "none";
+			function removeNode(){
+				iframe.onload = iframe.onerror = null;
+				iframe.parentNode && iframe.parentNode.removeChild(iframe);
+			}
+			iframe.onload = iframe.onerror = removeNode;
+			setTimeout(removeNode, 1000);
+		},
+		exec: function(method, params){
+			var Bridge = this;
+			return new Promise(function(resolve, reject){
+				Bridge._exec(method, params, function(result){
+					var error = result && result.error;
+					var fail = params.fail;
+					var success = params.success;
+					if(error){
+						error = new Error(error);
+						fail && fail(error);
+						reject(error);
+						Bridge.onerror && Bridge.onerror(error);
+					}else{
+						success && success(result);
+						resolve(result);
+					}
+				});
+			});
+		}
+	};
+});
+
